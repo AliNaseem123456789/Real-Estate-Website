44ac5e4322e362e114d828e19d32b0fc8dcc1b3a
alinaseem123 <alinaseem21102002@gmail.com>
2023-12-25 15:45:00 +0500

    feat(api): implement middleware for property listing validation and security

--- api/testcinnection.js
+++ api/testcinnection.js
@@ -1,16 +1,43 @@
 import mongoose from "mongoose";
+import PropertyModel from "./models/property.js";
+// ⭐ Define the MongoDB URL
+const MONGO_URL =
+  "mongodb+srv://alinaseem20021021_db_user:ali123456789@cluster0.j16gmhw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0";
 
-const uri = "mongodb+srv://alinaseem20021021_db_user:ali123456789@cluster0.j16gmhw.mongodb.net/mydatabase?retryWrites=true&w=majority";
+async function run() {
+  try {
+    // ⭐ Connect ONCE
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-mongoose.connect(uri)
-  .then(() => {
-    console.log("✅ Successfully connected to MongoDB Atlas!");
-    return mongoose.connection.db.listCollections().toArray();
-  })
-  .then(collections => {
-    console.log("Collections in mydatabase:", collections.map(c => c.name));
-    mongoose.disconnect();
-  })
-  .catch(err => {
-    console.error("❌ MongoDB connection error:", err);
-  });
+    console.log("Connected to MongoDB");
+
+    // Get all properties sorted by creation order
+    const properties = await PropertyModel.find({}).sort({ _id: 1 });
+
+    console.log(`Found ${properties.length} properties`);
+
+    // Assign image_prefix 1, 2, 3, ...
+    for (let i = 0; i < properties.length; i++) {
+      const prefix = String(i + 1);
+
+      await PropertyModel.updateOne(
+        { _id: properties[i]._id },
+        { $set: { image_prefix: prefix } }
+      );
+
+      console.log(`Updated ${properties[i]._id} → prefix ${prefix}`);
+    }
+
+    console.log("All prefixes assigned!");
+    process.exit(0);
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
