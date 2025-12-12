b6ed43bd4193cbf1ed1b167edf0fdab4ad2c438d
alinaseem123 <alinaseem21102002@gmail.com>
2025-12-12 18:56:25 +0500

    real estate6

--- client/src/pages/complete.jsx
+++ client/src/pages/complete.jsx
@@ -21,7 +21,8 @@ export default function Complete() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001/properties/${id}`);
+        // const response = await axios.get(`http://localhost:3001/properties/${id}`);
+        const response = await axios.get(`https://real-estate-website-uvk2.onrender.com/properties/${id}`);
         setProperty(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
