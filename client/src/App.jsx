c79057e095c542f8b9b707077f7cc0f3de649fce
alinaseem123 <alinaseem21102002@gmail.com>
2025-12-12 20:32:11 +0500

    real estate7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
